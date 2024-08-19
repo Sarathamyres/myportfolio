b5fb4fd8f84ece481de14cdade96bc8626543374
Sarathamyres <thamyressara071@gmail.com>
2024-08-19 11:44:25 -0300

    arrumando pastas

---
+++ src/js/index.js
@@ -0,0 +1,24 @@
+document.querySelectorAll("button[data-popup]").forEach((button) => {
+  button.addEventListener("click", function () {
+    const popupId = this.getAttribute("data-popup");
+    document.getElementById(popupId).style.display = "flex";
+    document.body.classList.add("no-scroll");
+
+    document.getElementById("hide").setAttribute("data-popup-id", popupId);
+  });
+});
+
+document.querySelectorAll(".icone-hide").forEach((hide) => {
+  hide.addEventListener("click", function () {
+    const popupId = this.getAttribute("data-popup-id");
+    document.getElementById(popupId).style.display = "none";
+    document.body.classList.remove("no-scroll");
+  });
+});
+
+window.addEventListener("scroll", function () {
+  const barsList = querySelector(".navegation-bars");
+  if (window.pageYOffset > 80) {
+    barsList.classList.add('no-visible');
+  } 
+});
